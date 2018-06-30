74deefa8f3efca38c2462663342d567648b79812
Fabien JUIF <fabien.juif@gmail.com>
2018-06-30 20:33:55 +0200

    :sparkles: draw players

--- client/src/index.js
+++ client/src/index.js
@@ -3,16 +3,27 @@ import store from './store'
 
 const app = new Application()
 
-const rectangle = new Graphics();
-rectangle.lineStyle(4, 0xFF3300, 1);
-rectangle.beginFill(0x66CCFF);
-rectangle.drawRect(0, 0, 64, 64);
-rectangle.endFill();
-rectangle.x = 170;
-rectangle.y = 170;
-app.stage.addChild(rectangle);
+
+const printPlayers = () => {
+  const players = store.data.players.getAsArray()
+
+  players.forEach(({ position }) => {
+    console.log('draw a player')
+    const rectangle = new Graphics();
+    rectangle.lineStyle(4, 0xFF3300, 1);
+    rectangle.beginFill(0xff0000);
+    rectangle.drawRect(0, 0, 64, 64);
+    rectangle.endFill();
+    rectangle.x = position.x;
+    rectangle.y = position.y;
+    app.stage.addChild(rectangle);
+  })
+}
 
 // FIXME: don't use setTimeout, but wait the document to be ready
 setTimeout(() => {
   window.document.body.appendChild(app.view)
+
+  printPlayers()
+  store.subscribe(printPlayers)
 }, 1000)
--- server/src/game/addPlayer.js
+++ server/src/game/addPlayer.js
@@ -13,13 +13,23 @@ const USERNAMES = [
   'DietBanshee',
 ]
 
+const random = max => Math.round(Math.random() * max)
+
 module.exports = [
   when('@@io/connect')(({ socketId }, store, { io }) => {
-    const username = USERNAMES[Math.round(Math.random() * (USERNAMES.length - 1))]
-
-    store.data.players.add({ id: socketId, username })
+    const username = USERNAMES[random(USERNAMES.length - 1)]
 
     io.emit(socketId)({ type: '@@krf/SET>CLIENT>ID', payload: socketId })
+    io.emit(socketId)({ type: '@@krf/SET>DATA>PLAYERS', payload: store.data.players.getAsArray() })
+
+    store.data.players.add({
+      id: socketId,
+      username,
+      position: {
+        x: random(800),
+        y: random(600),
+      },
+    })
   }),
 
   when('@@io/disconnect')(({ socketId }, store) => {
